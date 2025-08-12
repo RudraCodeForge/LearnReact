0ec8dedcae784b3d8482340a6ced0913c299c01a
Prince Daksh <33217820-prince2113kumar@users.noreply.replit.com>
2025-08-12 09:17:38 +0000

    Add functionality in calculator

--- Projects/Calculator/src/components/Buttons.jsx
+++ Projects/Calculator/src/components/Buttons.jsx
@@ -1,11 +1,19 @@
 import styles from '../styles/Buttons.module.css';
 const Buttons = (props) => {
+  const GETBUTTON = (e,button) =>{
+    props.handleButtonClick(button);
+  }
   return (
     <div className={styles.buttonGrid}>
       {props.buttons.map((button, index) => (
         <button
           key={index}
-          className={`btn ${styles.calcButton} ${styles[button.type]} ${button.span ? styles[`span${button.span}`] : ""}`}
+          className={`btn ${styles.calcButton} 
+          ${styles[button.type]} 
+          ${button.span ? styles[`span${button.span}`] : ""}`}
+          onClick={(e)=>{
+            GETBUTTON(e,button)
+          }}
         >
           {button.text}
         </button>
--- Projects/Calculator/src/components/Display.jsx
+++ Projects/Calculator/src/components/Display.jsx
@@ -1,11 +1,11 @@
-import styles from '../styles/Display.module.css';
+import  styles from '../styles/Display.module.css';
 const Display = (props) => {
   return (
     <div className={`${styles.display} mb-4`}>
       <input
         type="text"
         className={`form-control ${styles.displayInput}`}
-        value={props.value}
+        value={props.display}
         readOnly
       />
     </div>
