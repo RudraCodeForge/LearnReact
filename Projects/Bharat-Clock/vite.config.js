c78bf39a7805b8a6727376c84beeeaa8cc5d3cc6
Prince Daksh <33217820-prince2113kumar@users.noreply.replit.com>
2025-08-27 20:19:15 +0000

    Update vite

--- Projects/Bharat-Clock/vite.config.js
+++ Projects/Bharat-Clock/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "./",   // 👈 ye sabse important hai GitHub Pages ke liye
   plugins: [react()],
   server: {
     host: '0.0.0.0',
